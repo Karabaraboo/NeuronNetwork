d8b2756678c8ff63fa932e04699c692c61eab744
Karabaraboo <toto-93@andex.ru>
2022-08-26 23:03:03 +0300

    Ready for a first launch

--- Main_Network.py
+++ Main_Network.py
@@ -3,4 +3,20 @@ import Mnist_loader # For Mnist downloading
 import Network
 
 # Downloading of Mnist_data
-training_data, validation_data, test_data = Mnist_loader.load_data()
+training_data, validation_data, test_data = Mnist_loader.load_data_wrapper()
+
+# Creating of Network
+'''
+An input pictures are 28*28 pixels. Therefor an input layer consist of 784 neurons.
+30 neurons of hidden layer
+10 neurons in output layer. Each neuron corresponds to decimal number from 0 to 9
+'''
+net = Network([784, 30, 10])
+
+# Trainig of Network
+'''
+30 epochs,
+mini_branch size is 10
+training velocity is 3.0
+'''
+net.SGD(training_data, 30, 10, 3.0, test_data = test_data)
